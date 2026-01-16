d8ca960789e9dd0c790d81da65bfa1703d69f387
carlos <carlosres1995@gmail.com>
2026-01-15 21:27:45 -0500

    FEAT: create all classes in the game

---
+++ script/juego.js
@@ -0,0 +1,35 @@
+class Juego {
+  constructor(alma, preguntas) {
+    this.alma = alma;
+    this.anubis = new Anubis();
+    this.preguntas = preguntas;
+    this.preguntasUsadas = [];
+    this.preguntaActual = null;
+  }
+
+  iniciar() {
+    console.log(this.anubis.hablar("El juicio ha comenzado."));
+    this.siguientePregunta();
+  }
+
+  siguientePregunta() {
+    if (this.preguntas.length === 0) {
+      this.finalizar();
+      return;
+    }
+
+    const index = Math.floor(Math.random() * this.preguntas.length);
+    this.preguntaActual = this.preguntas.splice(index, 1)[0];
+    this.preguntasUsadas.push(this.preguntaActual);
+  }
+
+  responder(indiceOpcion) {
+    const impacto = this.preguntaActual.obtenerImpacto(indiceOpcion);
+    this.alma.responder(impacto);
+    this.siguientePregunta();
+  }
+
+  finalizar() {
+    console.log(this.anubis.juzgar(this.alma));
+  }
+}
