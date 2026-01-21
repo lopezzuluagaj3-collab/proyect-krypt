ef8bcef725a53998be3b3ce614e5971d25e4db72
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-01-21 16:48:13 -0500

    Feat:add a balance with funcionality in main.html

--- script/Alma.js
+++ script/Alma.js
@@ -31,4 +31,20 @@ class Alma {
 
     return this.estado;
   }
+
+  Judge(){
+    const arm = document.getElementById("armWrapper");
+    const hL = document.getElementById("leftHanger");
+    const hR = document.getElementById("rightHanger");
+
+    const l = this.pureza;
+    const r = 100 - this.pureza;
+
+    const dif = l - r;
+    const angle = dif * 0.3;
+
+    arm.style.transform = `rotate(${angle}deg)`;
+    hL.style.transform = `rotate(${-angle}deg)`;
+    hR.style.transform = `rotate(${-angle}deg)`;
+  }
 }
